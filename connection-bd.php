129eaffa5d270fc9712f814a1d61f667a4136fe3
gnikusor <67011317+gnikusor@users.noreply.github.com>
2020-07-26 21:33:24 +0200

    Update connection-bd.php

--- connection-bd.php
+++ connection-bd.php
@@ -4,13 +4,9 @@ $usuario = 'root';
 $pass = '';
 
 try {
+    
     $pdo= new PDO($link, $usuario, $pass);
-
-    // foreach($pdo->query('SELECT * from images') as $fila) {
-    //     print_r($fila);
-    // }
    
-
 } catch (PDOException $e) {
     print "¡Error!: " . $e->getMessage() . "<br/>";
     die();
